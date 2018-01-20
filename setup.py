68f6faaa45a0c6d40243e9613c9743e08006ead9
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-01-20 18:43:27 +0300

    [Test] find_packages https://stackoverflow.com/a/28372869/5951529

--- setup.py
+++ setup.py
@@ -6,11 +6,13 @@ https://github.com/pypa/sampleproject
 """
 
 # Always prefer setuptools over distutils
+from setuptools import find_packages
 from setuptools import setup
 
 # For Markdown README
 # https://stackoverflow.com/a/26737672/5951529
 setup(
+    packages=find_packages(),
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
